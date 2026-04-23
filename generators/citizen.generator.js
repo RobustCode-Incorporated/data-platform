dc8972968df1f47d5d29793477ec0f3f1388e009
RobustCode Inc. <robustcodeinc@gmail.com>
2026-04-23 18:42:35 +0000

    Update communes config to Brussels municipalities

--- generators/citizen.generator.js
+++ generators/citizen.generator.js
@@ -14,7 +14,7 @@ export async function createCitizen() {
         postnom: faker.person.lastName(),
         prenom: faker.person.firstName(),
         dateNaissance: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
-        sexe: Math.random() > 0.5 ? 'M' : 'F',
+        sexe: Math.random() > 0.5 ? 'Homme' : 'Femme',
         lieuNaissance: faker.location.city(),
         numeroUnique: generateNumeroUnique(),
         password: faker.internet.password(),
